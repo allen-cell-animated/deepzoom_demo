a63a27c6ddf29567a5410edec2436d392cd0ee72
dmt <danielt@alleninstitute.org>
2017-09-13 09:15:46 -0700

    update for data set 1.1.0

--- tiles2/getfiles.py
+++ tiles2/getfiles.py
@@ -11,12 +11,15 @@ dest_dir = './src/'
 
 # just get ALL files from this dir into a flat list.  name collisions will be overwritten
 def pull_files(dataset):
-    imgpath = 'Z:/Allen-Cell-Explorer/Allen-Cell-Explorer_1.0.1/Cell-Viewer_Thumbnails/' + dataset + '/'
+    imgpath = '//allen/aics/animated-cell/Allen-Cell-Explorer/Allen-Cell-Explorer_1.1.0/Cell-Viewer_Thumbnails/' + dataset + '/'
     for dirpath, dirnames, filenames in os.walk(imgpath):
         for filename in [f for f in filenames if len(f.split('_')) == 3]:
             shutil.copy(os.path.join(dirpath, filename), dest_dir)
 
 pull_files('2017_03_08_Struct_First_Pass_Seg')
+pull_files('2017_05_15_tubulin')
+pull_files('2017_06_28_lamin')
+pull_files('2017_07_21_Tom20')
 
 # randomize the files list by renaming them
 for dirpath, dirnames, filenames in os.walk(dest_dir):
--- tiles2/readme.txt
+++ tiles2/readme.txt
@@ -1,7 +1,8 @@
 python getfiles.py
 magick montage -background "#000000" src\*.png .\cells.png
-# examine image and add/remove cells to make it nice and square without empty space
-# copy cells.png into ..\generator\deepzoom.py\examples\helloworld
+# examine image and add/remove cells from src\*.png to make it nice and square without empty space
+# then repeat above magick step
+copy cells.png ..\generator\deepzoom.py\examples\helloworld
 cd ..\generator\deepzoom.py\examples\helloworld
 python helloworld.py
 # copy cells.dzi and cells_files into ./tiles subdir where index.html is pointing to it.
