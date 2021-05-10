0c51de44e4fa86b1faf532d9c7802c75f31871b4
Daniel Toloudis <danielt@alleninstitute.org>
2021-05-09 17:11:27 -0700

    allow for more cell images

--- tiles2/getfiles.py
+++ tiles2/getfiles.py
@@ -7,7 +7,7 @@ import shutil
 
 
 dest_dir = './src/'
-imgpath = '//allen/aics/animated-cell/Allen-Cell-Explorer/Allen-Cell-Explorer_1.2.0/Cell-Viewer_Thumbnails/'
+imgpath = '//allen/aics/animated-cell/Allen-Cell-Explorer/Allen-Cell-Explorer_2_test/Cell-Viewer_Thumbnails/'
 
 # just get ALL files from this dir into a flat list.  name collisions will be overwritten
 def pull_files(dataset):
@@ -22,5 +22,5 @@ for dirpath, dirnames, filenames in os.walk(imgpath):
 # randomize the files list by renaming them
 for dirpath, dirnames, filenames in os.walk(dest_dir):
     for filename in [f for f in filenames]:
-        newname = str(random.randint(0,99999)).zfill(5) + filename
+        newname = str(random.randint(0,999999)).zfill(6) + filename
         os.rename(os.path.join(dirpath, filename), os.path.join(dirpath, newname))
